964b2200dc10eb04a83bdae2bc6644ec2396e008
Michael Vega <ichigo509@yahoo.com>
2019-10-28 14:02:28 -0400

    Updates

---
+++ precache-manifest.1928bf79da16af53947e627e80baa4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593ce3148dd214e795421e4c8cc85ed3",
+    "url": "/Google-Books-React/index.html"
+  },
+  {
+    "revision": "843aa633d481a72837cd",
+    "url": "/Google-Books-React/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "02f6a9add93cc0c4b6b4",
+    "url": "/Google-Books-React/static/js/2.de1d57a4.chunk.js"
+  },
+  {
+    "revision": "843aa633d481a72837cd",
+    "url": "/Google-Books-React/static/js/main.d186cbfd.chunk.js"
+  },
+  {
+    "revision": "8f0e47e0fe9179439555",
+    "url": "/Google-Books-React/static/js/runtime-main.801b8c97.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Google-Books-React/static/media/logo.25bf045c.svg"
+  }
+]);
